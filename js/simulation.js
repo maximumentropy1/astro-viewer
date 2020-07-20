23fa1540a2ae5e2542f12e6002ad078d9e1725d7
Max <kuhlmaxt@gmail.com>
2020-07-20 15:35:57 -0400

    add legend back

--- js/simulation.js
+++ js/simulation.js
@@ -1,5 +1,7 @@
 //initialize variables for canvas and jsdataframe for dealing with data about objects
 var canvas = document.getElementById("simulation");
+var textbox = document.getElementById("description");
+textbox.style.display = "none";
 var ctx = canvas.getContext("2d");
 
 window.addEventListener('resize', resizeCanvas, false);
@@ -18,7 +20,7 @@ var state = {
     coords: {x: 0, y: 0},
     ra: {raw: 0, hour: 0, arcmin: 0, arcsec: 0},
     d: {raw: 0, deg: 0, arcmin: 0, arcsec: 0},
-    //tracks what object the description is currently about
+    showObj: false,
     objNum: 0,
     //this variable is to tell if the mouse was clicked not on one of the objects to turn showObj off
     anyObjChange: false
@@ -30,16 +32,14 @@ img.src = '../images/betterstarmap.png';
 img.onload = function(){
     ctx.drawImage(img, state.coords.x, state.coords.y);
 };
-
-//deprecated:
-//var objectImages = {};
-//function addImage(object) {
-//    //create each image and load it into the array
-//    var newImage = new Image(100, 100);
-//    newImage.src = object.imgLink;
-//    objectImages[object.objName] = newImage;
-//};
-//objects.forEach(addImage);
+var objectImages = {};
+function addImage(object) {
+    //create each image and load it into the array
+    var newImage = new Image(100, 100);
+    newImage.src = object.imgLink;
+    objectImages[object.objName] = newImage;
+};
+objects.forEach(addImage);
 
 function clear() {
     // Store the current transformation matrix
@@ -51,69 +51,19 @@ function clear() {
     ctx.restore();
 }
 
-//deprecated:
-//function that auto wraps text for the descriptions
-//function wrapText(text, x, y, maxWidth, lineHeight) {
-//    var words = text.split(' ');
-//    var line = '';
-//    //loops through the words testing each one if it is too long
-//    for(var n = 0; n < words.length; n++) {
-//        var testLine = line + words[n] + ' ';
-//        var metrics = ctx.measureText(testLine);
-//        var testWidth = metrics.width;
-//        if (testWidth > maxWidth && n > 0) {
-//            ctx.fillText(line, x, y);
-//            line = words[n] + ' ';
-//            y += lineHeight;
-//        } else {
-//            line = testLine;
-//        }
-//    }
-//    ctx.fillText(line, x, y);
-//}
 
-function changeDescription(objectID) {
-    //this code should run once whenever the description needs to change
+function drawObject(objectID) {
     object = objects[objectID];
     
     //draw the object
 
     //draw rectangle behind image
-    //ctx.beginPath();
+    ctx.beginPath();
     //set style:
-    //ctx.lineWidth = "0";
-    //ctx.fillStyle = "rgba(0,0,0,0.8)";
-    //draw rectangle
-    //ctx.rect(0, canvas.height - 500, 500, 500);
-    //ctx.fill();
-
-    //ctx.drawImage(objectImages[object.objName], 30, canvas.height - 300, 0.3*objectImages[object.objName].naturalWidth, 0.3*objectImages[object.objName].naturalHeight);
-
-    //laggy code for images - loads a new one each time it is requested - even when it eventually ends up in the browser's cache the image still takes a few seconds to show up the first time
-    //var image = new Image();
-    //image.src = objectImages[objects[state.objNum].objName].src;
-    //ctx.drawImage(image, 30, canvas.height - 330, 0.25*image.naturalWidth, 0.25*image.naturalHeight);
-    
-    
-    var descriptionContent = document.getElementById("content");
-    //if the description content is not already correct, change it to be correct
-    if(descriptionContent.innerHTML != object.description) {
-	descriptionContent.innerHTML = object.description;
-    }
-
-    //if the img is not correct, change it
-    var descriptionImg = document.getElementById("descriptionImg");
-    if(descriptionImg.src != object.imgLink) {
-	descriptionImg.src = object.imgLink;
-    }
-    //deprecated code for descriptions:
-    //draw title and description text:
-    //ctx.font = "20px Arial";
-    //ctx.fillStyle = "white";
-    //wrapText(object.objName, 30, canvas.height - 450, 440, 20);
-
-    //ctx.font = "15px Arial";
-    //wrapText(object.description, 30, canvas.height - 420, 440, 20);
+    ctx.lineWidth = "0";
+    ctx.fillStyle = "rgba(0,0,0,0.8)";
+    //draw object image
+    ctx.drawImage(objectImages[object.objName], canvas.width-450, canvas.height - 450, 0.4*objectImages[object.objName].naturalWidth, 0.4*objectImages[object.objName].naturalHeight);
 }    
 function drawDots(object) {
     var x = object.x + state.coords.x
@@ -186,7 +136,7 @@ function draw() {
     ctx.beginPath();
     ctx.fillStyle = "rgba(0,0,0,0.8)";
     //draw rctangle
-    ctx.rect(canvas.width - 200, 0, 200, 130);
+    ctx.rect(canvas.width - 200, 0, 200, 150);
     ctx.fill();
     //draw legend
     ctx.fillStyle = "rgb(100, 150, 200)";
@@ -212,24 +162,30 @@ function draw() {
     ctx.arc(x, y + 40, 15, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.fill();
-    //draw triangle (misc)
+    //draw triangle
     ctx.beginPath();
     ctx.linewidth = "0";
     ctx.moveTo(x - 15*Math.cos(Math.PI/6), 80 + y - 15*Math.sin(Math.PI/6));
     ctx.lineTo(x + 15*Math.cos(Math.PI/6), 80 + y - 15*Math.sin(Math.PI/6));
     ctx.lineTo(x, 80 + y + 15);
     ctx.closePath();
-    ctx.fill();
+    ctx.fill();	
     //draw text
     ctx.font = "20px Arial";
     ctx.fillStyle = "white";
     ctx.fillText("= star", x + 30, y + 8);
-    ctx.fillText("= planet", x + 30, y + 48);
-    ctx.fillText("= miscellaneous", x + 30, y + 88);
+    ctx.fillText("= planet", x + 30, y + 50);
+    ctx.fillText("= miscellaneous", x + 30, y + 92);
+
+    
+    //draw an object if it says too
+    if(state.showObj) {
+	drawObject(state.objNum);
+    }
 };
 
 //when mouse is pressed, store coordinates and set move variable to true so it moves
-window.addEventListener('mousedown', function(event) {
+canvas.addEventListener('mousedown', function(event) {
     state.mouse = {
         x: event.pageX - canvas.offsetLeft,
         y: event.pageY - canvas.offsetTop
@@ -240,11 +196,11 @@ window.addEventListener('mousedown', function(event) {
 })
 
 //when mouse is released, stop moving around the image
-window.addEventListener('mouseup', function(event) {
+canvas.addEventListener('mouseup', function(event) {
     state.drag = false;
 })
 
-window.addEventListener('mousemove', function(event) {
+canvas.addEventListener('mousemove', function(event) {
     state.mouse = {
         x: event.pageX - canvas.offsetLeft,
         y: event.pageY - canvas.offsetTop
@@ -273,38 +229,48 @@ window.addEventListener('mousemove', function(event) {
 })
 
 function collisions(item, index) {
-    //code for descriptions:
-    var description = document.getElementById("description");
-
     //if it the mouse is above the object
     if(item.x + 20 + state.coords.x > state.mouse.x && item.x - 20 +state.coords.x< state.mouse.x && item.y + 20 + state.coords.y> state.mouse.y && item.y - 20 + state.coords.y < state.mouse.y) {
 	state.anyObjChange = true;
 	item.visited = true;
 	if(state.objNum == index) {
-	    //change if it should be visible
-	    description.classList.toggle('show');
+	    //if the variable is already set, toggle whether to show it or not
+	    state.showObj = !state.showObj;
 	} else {
-	    description.classList.add('show');
+	    state.showObj = true;
 	    state.objNum = index;
 	}
-	changeDescription(index);
     }
 }
-
+function addDescription() {
+    var textbox = document.getElementById("description");
+    var node = document.createTextNode(objects[state.objNum].description);
+    textbox.appendChild(node);
+    textbox.style.display = "block";
+}
 canvas.addEventListener('click', function(event) {
     objects.forEach(collisions);
 
     //check if any change occured, if not turn off the display object function
     if(!state.anyObjChange) {
-	document.getElementById("description").classList.remove("show");
+	state.showObj = false;
     } else { //reset the variable for the next run
 	state.anyObjChange = false;
     }
     
     //make sure to draw the panel again in case it needs to cover up the old one or draw a new one
     draw();
+    console.log(state.showObj);
+    console.log(textbox);
+    if (state.showObj) {
+        addDescription();
+    }
+    else {
+        textbox.innerHTML = '';
+        textbox.style.display = "none";
+    }
 });
-	
+
 window.addEventListener('resize', resizeCanvas, false);
 
 resizeCanvas();
